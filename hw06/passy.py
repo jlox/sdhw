b48448658689d701dfb7e4bb1c020cffc8290ced
Jeanne Locker <jeannelocker@earthlink.net>
2016-04-01 09:57:51 -0400

    yay homeworkiesgit add hw06/passy.py hw07/closures.py!

--- hw06/passy.py
+++ hw06/passy.py
@@ -1,13 +1,64 @@
 
+def good(p):
+    lower="abcdefghijklmnopqrstuvwxyz"
+    upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ints="0123456789"
+    l = [1 for x in p if x in lower]
+    u = [1 for x in p if x in upper]
+    i = [1 for x in p if x in ints]
+
+    lo = False
+    up = False
+    ind = False
+    
+    if 1 in l:
+        lo = True
+    if 1 in u:
+        up = True
+    if 1 in i:
+        ind = True
+    if (lo==True and up==True and ind==True):
+        print "you're good"
+    else:
+        print "you're missing something here"
+        
+good("Bunnies1*")
+good("cats")
+
 def massive(p):
-    lower="abcdefghijklmnopqrstuvwxyz" #val 0
-    upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ" #val 1
-    ints="0123456789" #val 2
-    result = [0 if x in lower else 1 if x in upper else 2 for x in p]  
+    lower="abcdefghijklmnopqrstuvwxyz"
+    upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ints="0123456789"
+    special=".?!&#,;:-_*"
+
+    l = [1 for x in p if x in lower]
+    u = [1 for x in p if x in upper]
+    i = [1 for x in p if x in ints]
+    s = [1 for x in p if x in special]
+    print l
+    print u
+    print i
+    print s
+
+    lo = 0
+    up = 0
+    ind = 0
+    spec = 0
+    if 1 in l:
+        lo += 1
+    if 1 in u:
+        up += 2
+    if 1 in i:
+        ind += 3
+    if 1 in s:
+        spec +=4
+    print (spec+ind+up+lo)
     
 
+massive("Bunnies3*")
+
+
     
-#massive("Bunnies1*")
 '''print [x for x in range(8)]
 print [x*x for x in range(8)]
 p="myNoobPass1234"
---
+++ hw07/closures.py
@@ -0,0 +1,20 @@
+def repeat(thingy):
+    def g(y):
+        i = y
+        s = ''
+        while i > 0:
+            s = s + thingy
+            i -= 1
+        return s
+    return g
+
+print repeat('hi')(3)
+print repeat('three?')(4)
+
+r1 = repeat('hello')
+print r1(2)
+r2 = repeat('goodbye')
+print r2(2)
+#print r1(2) ->hellohello
+#print r2(2) -->goodbyegoodbye
+#print repeat('cool')(3) -> coolcoolcool
